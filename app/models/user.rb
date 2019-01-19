e5b7dda62f73418fc33cf3b723459bfe6b08fb7c
rob <robertchen234@gmail.com>
2019-01-19 16:08:12 -0500

    auth ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
+
   belongs_to :mod
   has_many :posts, dependent: :destroy
   has_many :likes
